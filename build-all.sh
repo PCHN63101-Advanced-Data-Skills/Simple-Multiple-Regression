f7d4e6d63bde50d159f00e2a1a7508cb33267d02
martynmcfarquhar <martynmcfarquhar@googlemail.com>
2025-04-07 13:01:51 +0100

    First commit of the regression lesson

--- build-all.sh
+++ build-all.sh
@@ -2,7 +2,9 @@
 
 rm -rf ./_build
 jupyter-book build ./
+echo Enter message for commit
+read commitMessage 
 git add -A
-git commit -m 'updates'
+git commit -m "$commitMessage"
 git push
 ghp-import -n -p -f _build/html
